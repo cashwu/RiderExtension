d3938f76245c258865a9ad15a3ba34870f6bff03
Cash Wu <cash@cashwu.com>
2019-01-13 23:06:28 +0800

    add dbcontext

---
+++ RiderExtension/Template.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+
+namespace RiderExtension
+{
+    public static class Template
+    {
+        [SourceTemplateAttribute]
+        [Macro(Target = "dbcontext", Editable = 1)]
+        public static void _dbcontext<T>(this T source)
+        {
+            /*$ 
+             services.AddDbContext<$dbcontext$>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"), optionsBuilder =>
+                {
+                    optionsBuilder.CommandTimeout(30);
+                    optionsBuilder.EnableRetryOnFailure(3, TimeSpan.FromSeconds(10), new List<string>());
+                    optionsBuilder.RemoteCertificateValidationCallback((sender, certificate, chain, errors) => true);
+                });
+                options.EnableSensitiveDataLogging();
+            });*/
+        }
+    }
+}
